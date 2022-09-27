227b0e0c3e72d2d00f2cae39e440feed94de49e1
Eng. CESARIO SILVA <jnsoftdev@users.noreply.github.com>
2022-09-27 11:08:18 -0300

    Initial commit

---
+++ app/src/main/java/br/com/jnsoft/mycad/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.jnsoft.mycad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
