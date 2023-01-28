75379652bd2cc76296702214e18412f8427d5e3c
Eng. CESARIO SILVA <jnsoftdev@users.noreply.github.com>
2023-01-27 23:27:22 -0300

    criação da bibliotecas constantes e Ultilitarios...
    dando inicio

---
+++ app/src/main/java/br/com/jnsoft/mycad/helper/Constant.java
@@ -0,0 +1,14 @@
+package br.com.jnsoft.mycad.helper;
+
+public class Constant {
+
+    public static final String ServerAddress = "192.168.1.51";
+    public static final String ServerPacth = "http://" + ServerAddress + "/services/barbershop/";
+    public static final String API_TOKEN = "COLOCA A CHAVE DA API";
+
+    public static String modo = "new";
+
+    //preencher com dados que pode ser usando a qualquer momento e de qualquer lugar pelo sistema!
+
+
+}
---
+++ app/src/main/java/br/com/jnsoft/mycad/helper/Utils.java
@@ -0,0 +1,22 @@
+package br.com.jnsoft.mycad.helper;
+
+public class Utils {
+
+    //Converte um inteiro ou texto em boolean válida! ex.: 0 = false; 1 = true; ...
+    public static Boolean isIntBool(String value){
+        value = value.toUpperCase().trim();
+        if(value.contains("1") || value.contains("TRUE")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //converte bool em int: false = 0; true = 1;
+    public static Integer isBoolInt(boolean value){
+        if(value == true){return 1;} else { return 0;}
+    }
+
+
+
+}
